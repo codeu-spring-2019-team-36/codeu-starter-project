62de417cdb496d4e3ad604b3a0ee4db09bd8d923
Faesal Murad <faesalm@uvic.ca>
2019-04-10 21:59:06 -0700

    stickler part 2

--- src/main/java/com/google/codeu/servlets/ItemDataServlet.java
+++ src/main/java/com/google/codeu/servlets/ItemDataServlet.java
@@ -2,14 +2,14 @@ package com.google.codeu.servlets;
 
 import com.google.codeu.data.Item;
 import com.google.gson.Gson;
+import com.google.appengine.api.users.UserService;
+import com.google.appengine.api.users.UserServiceFactory;
+import com.google.codeu.data.Datastore;
 import java.io.IOException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import com.google.appengine.api.users.UserService;
-import com.google.appengine.api.users.UserServiceFactory;
-import com.google.codeu.data.Datastore;
 import org.jsoup.Jsoup;
 import org.jsoup.safety.Whitelist;
 
@@ -28,7 +28,7 @@ public class ItemDataServlet extends HttpServlet {
   @Override
   public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
     String user = request.getParameter("user");
-    Item postingData = null;
+    Item postingData;
     try {
       postingData = datastore.getPosting(user);
     } catch (NullPointerException e) {
