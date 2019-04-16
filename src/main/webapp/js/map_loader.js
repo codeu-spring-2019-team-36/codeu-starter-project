127d41f74af10d894c373d4acdacdea17dba0bac
Rebecca Xu <rxu2460@gmail.com>
2019-04-16 00:57:23 -0700

    prettier

--- src/main/webapp/js/map_loader.js
+++ src/main/webapp/js/map_loader.js
@@ -1,24 +1,24 @@
 let map;
 function createMap() {
-  const map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 37.422403, lng: -122.088073},
+  const map = new google.maps.Map(document.getElementById("map"), {
+    center: { lat: 37.422403, lng: -122.088073 },
     zoom: 15
   });
 
   // Create the search box and link it to the UI element.
-  var input = document.getElementById('pac-input');
+  var input = document.getElementById("pac-input");
   var searchBox = new google.maps.places.SearchBox(input);
   //map.controls[google.maps.ControlPosition.TOP_RIGHT].push(input);
 
   // Bias the SearchBox results towards current map's viewport.
-  map.addListener('bounds_changed', function() {
+  map.addListener("bounds_changed", function() {
     searchBox.setBounds(map.getBounds());
   });
 
   var markers = [];
   // Listen for the event fired when the user selects a prediction and retrieve
   // more details for that place.
-  searchBox.addListener('places_changed', function() {
+  searchBox.addListener("places_changed", function() {
     var places = searchBox.getPlaces();
 
     if (places.length == 0) {
@@ -47,12 +47,14 @@ function createMap() {
       };
 
       // Create a marker for each place.
-      markers.push(new google.maps.Marker({
-        map: map,
-        icon: icon,
-        title: place.name,
-        position: place.geometry.location
-      }));
+      markers.push(
+        new google.maps.Marker({
+          map: map,
+          icon: icon,
+          title: place.name,
+          position: place.geometry.location
+        })
+      );
 
       if (place.geometry.viewport) {
         // Only geocodes have viewport.
